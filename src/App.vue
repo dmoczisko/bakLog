b89b49d02aaae5826b40935cc42fa9af0e4f3199
Dan Moczisko <dan.m@wizardsoftware.net>
2022-09-30 15:24:59 -0500

    testing tailwind implementation

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
+    <div class="wrapper bg-black">
       <HelloWorld msg="You did it!" />
 
       <nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.vue', 'index.html'],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
