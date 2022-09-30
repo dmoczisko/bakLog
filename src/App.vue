145497ef1608543efeaf471e48cc85d376e72fa9
Dan Moczisko <dan.m@wizardsoftware.net>
2022-09-30 15:32:44 -0500

    tailwind implemented and working

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import HelloWorld from './components/HelloWorld.vue';
 
 <template>
   <header>
-    <div class="wrapper bg-black">
+    <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
