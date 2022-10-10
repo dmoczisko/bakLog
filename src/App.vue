7a7a42466ca8d5e244cbc94f3b04397941fcb5ba
Dan Moczisko <danny.moczisko@gmail.com>
2022-10-09 19:56:04 -0500

    gitignore .env files

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,22 @@ import Header from './components/HeaderComponent.vue';
 import GamesList from './components/GamesListComponent.vue';
 import MostPlayed from './components/MostPlayedComponent.vue';
 import Footer from './components/FooterComponent.vue';
+
+//need to move to hidden, gitignored file
+fetch('https://url.com', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+  // body: JSON.stringify({
+  //   name: 'User 1'
+  // })
+})
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log('Error'));
 </script>
 
 <template>
