b80d1e7b398465b5f853b33c160fa0eb5648dc1c
Dan Moczisko <danny.moczisko@gmail.com>
2022-10-13 15:58:05 -0500

    adding basic search functionality

--- src/components/GamesListComponent.vue
+++ src/components/GamesListComponent.vue
@@ -1,4 +1,61 @@
 <template>
+  <div class="my-5 text-center text-3xl">
+    <h1>Search</h1>
+    <form
+      :disabled="!isSearchTextPresent"
+      @submit.prevent="searchSubmit"
+      class="flex items-center justify-center font-sans overflow-hidden px-16"
+    >
+      <label for="simple-search" class="sr-only">Search</label>
+      <div class="relative w-full">
+        <div
+          class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="text"
+          id="search-text"
+          v-model="searchText"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+          placeholder="Search"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        <svg
+          class="w-5 h-5"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          ></path>
+        </svg>
+        <span class="sr-only">Search</span>
+      </button>
+    </form>
+  </div>
   <h2 class="my-5 text-center text-3xl text-red-600">Games List</h2>
   <div class="overflow-x-auto">
     <div
@@ -34,8 +91,18 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import GameComponent from '@/components/Games/GameComponent.vue';
+
+const searchText = ref('');
+const isSearchTextPresent = computed(() => searchText.value.length > 0);
+
+function searchSubmit() {
+  const searchTextValue = searchText.value;
+  console.log(searchTextValue);
+  searchText.value = '';
+}
+
 //const currentYear = new Date().getFullYear();
 
 // Get years since release - refactor later
