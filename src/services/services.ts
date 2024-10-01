142bfb46dbba55e4491f80dae62579b961a8968e
Dan Moczisko <danny.moczisko@gmail.com>
2024-10-01 12:13:27 -0500

    Update packages and env var

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const BASE_URL = 'process.env.BASE_URL';
+const API_KEY = 'process.env.API_KEY';
 
 export const fetchGames = async () => {
   const response = await axios.get(`${BASE_URL}/games?key=${API_KEY}`);
